06f259bb2416d39100f004cc253ddd7f1853be62
kozinus <kkozin2403@gmail.com>
2023-07-08 13:52:14 +0300

    2. Создать контейнер на базе связанного списка  [#159  [#159 #428628]]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,70 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+
+    private int size;
+    private int modCount;
+    private final Node<E> head = new Node<>(null, null);
+
+    @Override
+    public void add(E value) {
+        Node<E> last = head;
+        for (int i = 0; i < size; i++) {
+            last = last.next;
+        }
+        last.item = value;
+        last.next = new Node<>(null, null);
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> last = head;
+        for (int i = 0; i < index; i++) {
+            last = last.next;
+        }
+        return last.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            final int expectedModCount = modCount;
+            Node<E> last = new Node<>(null, head);
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return last.next.next != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                last = last.next;
+                return last.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
