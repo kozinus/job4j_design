b22d382cb696e80f2341e4933cdadd3340a6c320
kozinus <kkozin2403@gmail.com>
2023-07-17 11:28:17 +0300

    7. Анализ доступности сервера [#859  [#859 #430314]]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,26 +5,27 @@ import java.util.function.Consumer;
 
 
 public class Analysis {
-    private static boolean flag = true;
 
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             PrintWriter out = new PrintWriter(
                     new BufferedOutputStream(
-                            new FileOutputStream(target)
-                    ));
-            Consumer<String> lineAction = x -> {
-                String[] s = x.split(" ", 2);
-                boolean isAvailable = "400".equals(s[0]) || "500".equals(s[0]);
-                if (!isAvailable && flag) {
+                            new FileOutputStream(target)));
+            boolean flag = true;
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] s = line.split(" ", 2);
+                boolean isUnvailable = "400".equals(s[0]) || "500".equals(s[0]);
+                if (isUnvailable && flag) {
                     flag = false;
                     out.print(s[1] + ";");
-                } else if (isAvailable && !flag) {
+                } else if (!isUnvailable && !flag) {
                     flag = true;
                     out.print(s[1] + ";" + System.lineSeparator());
                 }
-            };
-            in.lines().forEach(lineAction);
+            }
+            in.close();
+            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
