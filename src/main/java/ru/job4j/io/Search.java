c367e58917125e773799666ce3efd3594f87e27a
kozinus <kkozin2403@gmail.com>
2023-07-22 15:24:58 +0300

    12. Валидация параметров запуска [#246865  [#246865 #431504]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,6 +9,17 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        validationOf2Arguments(args);
+        search(Path.of(args[0]), p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    private static void validationOf2Arguments(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is not full");
         }
@@ -19,12 +30,5 @@ public class Search {
         if (!args[1].startsWith(".")) {
             throw new IllegalArgumentException(String.format("Second argument %s is not an extension", args[1]));
         }
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-    }
-
-    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles searcher = new SearchFiles(condition);
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
     }
 }
