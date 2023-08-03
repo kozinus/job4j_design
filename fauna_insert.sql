997b193ffda68df1d9b245a32ac7407d4199674b
kozinus <45349091+kozinus@users.noreply.github.com>
2023-08-03 21:23:11 +0300

    0. Select [#6861 # [#6861]]

---
+++ fauna_insert.sql
@@ -0,0 +1,10 @@
+insert into fauna(name, avg_age, discovery_date) 
+values ('lion', 9000, '1623-02-21');
+insert into fauna(name, avg_age, discovery_date) 
+values ('pigeon', 1100, '1723-07-21');
+insert into fauna(name, avg_age, discovery_date) 
+values ('white whale', 18000, '1556-07-21');
+insert into fauna(name, avg_age) 
+values ('sword fish', 1800);
+insert into fauna(name, avg_age) 
+values ('crow', 11000);
