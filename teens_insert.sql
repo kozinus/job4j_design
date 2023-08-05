c23b130ba7d85deaa49f44a327d9a763ad1ecc67
kozinus <45349091+kozinus@users.noreply.github.com>
2023-08-05 19:53:08 +0300

    0. Join [#6862  [#6862 #434845]]

--- teens_insert.sql
+++ teens_insert.sql
@@ -1,10 +1,9 @@
 insert into teens(name, sex)
 values
-('Andrej', 0),
-('Maria', 1),
-('Varvara', 1),
-('Anastasia', 1),
-('Mark', 0),
-('lisa', 1);
-
-insert into teens(name) values ('unknown');
+('Andrej', false),
+('Maria', true),
+('Varvara', true),
+('Anastasia', true),
+('Mark', false),
+('lisa', true),
+('unknown', null);
