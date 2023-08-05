bebe3aa4d3ebdd4366a54ec18c9c12e9cc3e8a92
kozinus <45349091+kozinus@users.noreply.github.com>
2023-08-05 16:43:01 +0300

    2. Установка PostgreSQL [#55346  [#55346 #433903]]

--- create.sql
+++ create.sql
@@ -1,3 +1,45 @@
+create table rules (
+	id serial primary key,
+	rule varchar(255)
+);
+
+create table roles (
+	id serial primary key,
+	role_name varchar(255),
+	rule_id int references rules(id)
+);
+
+create table users (
+	id serial primary key,
+	name varchar(255),
+	role_id int references roles(id)
+);
+
+create table categories (
+	id serial primary key,
+	category varchar(255)
+);
+
+create table items (
+	id serial primary key,
+	item_name varchar(255)
+);
+
+create table coments (
+	id serial primary key,
+	comm_content varchar(255)
+);
+
+create table attachs (
+	id serial primary key,
+	files_names varchar(255)
+);
+
+create table states (
+	id serial primary key,
+	state varchar(255)
+);
+
 create table items_users (
 	id serial primary key,
 	item_id int references items(id),
